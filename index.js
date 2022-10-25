41148a8031dc2ae5e946362a26f00440da5e1c58
camilagv1 <mcgrajales071@misena.edu.co>
2022-10-25 16:53:56 -0500

    index

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const os = requiere('OS');
 const app = express();
 
 app.get('/',(req, res)=>{
-    res.send('Hola mundo')
+    res.send(`
+    <h1>Hola a todos</h1>
+    <h2>Mi nombre es tu outa ma</h2>
+    `)
 })
 
 app.listen(3000, () => {
